0c8b7cb6f46d7d773f66e31a4cee3ad63e0c30ac
Eric Chen <chen.ericsn@outlook.com>
2020-02-20 15:38:37 +0800

    new auto commands

---
+++ code8011_20200204/src/main/cpp/auto/auto_commands.cpp
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "auto/auto_commands.h"
+
+auto_commands::auto_commands() {
+
+    void auto_commands::move(double speed, double targetDist, double heading){
+
+    }
+
+}
+
